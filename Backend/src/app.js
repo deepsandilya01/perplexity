1d62032fcdd82c4846f472f090db584ddc9c5eec
deepsandilya01 <deepsandilya23@gamil.com>
2026-03-21 16:14:49 +0530

    Add initial HTML template for the application

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,6 +4,11 @@ import authRouter from "./routes/auth.routes.js";
 import chatRouter from "./routes/chat.routes.js";
 import morgan from "morgan";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url"; // ✅ add this
+
+const __filename = fileURLToPath(import.meta.url); // ✅ add this
+const __dirname = path.dirname(__filename); // ✅ add this
 
 const app = express();
 
@@ -14,7 +19,7 @@ app.use(cookieParser());
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   }),
@@ -25,7 +30,19 @@ app.get("/", (req, res) => {
   res.json({ message: "Server is running" });
 });
 
+
+
+// ✅ static files first
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use("/api/auth", authRouter);
 app.use("/api/chats", chatRouter);
+
+console.log(__dirname);
+
+// ✅ catch-all LAST
+app.get("*name", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 export default app;
